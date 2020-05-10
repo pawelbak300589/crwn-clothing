ac9a7a4f81110636784404bc97c97202cec2ae3f
Pawel Bak <pawelbak300589@gmail.com>
2020-05-10 08:47:34 +0100

    Add files for homepage

---
+++ src/homepage.container.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+import './homepage.styles.scss';
+
+const HomePage = () => {
+    return (
+        <div className="homepage">
+            <div className="direcroty-menu">
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">HATS</h1>
+                        <span className="subtitles">SHOP NOW</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">HATS</h1>
+                        <span className="subtitles">SHOP NOW</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">JACKETS</h1>
+                        <span className="subtitles">SHOP NOW</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">SNICKERS</h1>
+                        <span className="subtitles">SHOP NOW</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">WOMENS</h1>
+                        <span className="subtitles">SHOP NOW</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">MENS</h1>
+                        <span className="subtitles">SHOP NOW</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default HomePage;
