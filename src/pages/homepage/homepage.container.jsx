82fe3ddb536a61e0bfffe57c3ac551624de9a3cf
Pawel Bak <pawelbak300589@gmail.com>
2020-05-10 09:43:26 +0100

    Broken homepage into smaller components, finished styling menu-item component

---
+++ src/pages/homepage/homepage.container.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Directory from "../../components/directory/directory.container";
+
+import './homepage.styles.scss';
+
+const HomePage = () => {
+    return (
+        <div className="homepage">
+            <Directory />
+        </div>
+    );
+};
+
+export default HomePage;
