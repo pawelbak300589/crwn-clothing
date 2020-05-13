ef5f2e29d57e4e9879096a6f07d8a57926e0abdc
Pawel Bak <PawelBak@staveleyhead.co.uk>
2020-05-13 14:23:54 +0100

    Checkpoint - lesson 124

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import './App.css';
 import HomePage from "./pages/homepage/homepage.container";
 import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import CheckoutPage from "./pages/checkout/checkout.component";
+
 import Header from "./components/header/header.component";
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 import { setCurrentUser } from "./redux/user/user.actions";
@@ -45,7 +47,8 @@ class App extends React.Component {
                 <Header />
                 <Switch>
                     <Route exact path="/" component={HomePage} />
-                    <Route exact path="/shop" component={ShopPage} />
+                    <Route path="/shop" component={ShopPage} />
+                    <Route exact path="/checkout" component={CheckoutPage} />
                     <Route exact
                            path="/signin"
                            render={() => this.props.currentUser ? (<Redirect to="/" />) : (<SignInAndSignUpPage />)} />
