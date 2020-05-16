50e54de7a2817278c3f426027f665450d747a3e0
Pawel Bak <pawelbak300589@gmail.com>
2020-05-16 10:46:04 +0100

    Add stripe integration and update favicon and app title

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+import StripeCheckoutButton from "../../components/stripe-button/stripe-button.component";
 
 import { selectCartItems, selectCartTotal } from "../../redux/cart/cart.selectors";
 
@@ -31,10 +32,17 @@ const CheckoutPage = ({ cartItems, total }) => {
             {
                 cartItems.map(cartItem => (<CheckoutItem key={cartItem.id} cartItem={cartItem} />))
             }
-
-            <div className="total">
-                <span>TOTAL: ${total}</span>
+            <div className="total"><span>TOTAL: ${total}</span></div>
+            <div className="test-warning">
+                *Please use the following test credit card for payments*
+                <br />
+                Card Number: 4242 4242 4242 4242
+                <br />
+                CVC: Any 3 digits
+                <br />
+                Exp: Any future date
             </div>
+            <StripeCheckoutButton price={total} />
         </div>
     );
 };
