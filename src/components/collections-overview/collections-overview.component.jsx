18e5fa4baca6bc794d9d9d5c3bf164b8f273af47
Pawel Bak <pawelbak300589@gmail.com>
2020-05-28 10:03:37 +0100

    Updating styles to use media queries for mobile

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -1,24 +1,19 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { createStructuredSelector } from "reselect";
+import { createStructuredSelector } from 'reselect';
 
-import CollectionPreview from "../../components/collection-preview/collection-preview.component";
+import CollectionPreview from '../collection-preview/collection-preview.component';
 
-import { selectCollectionsForPreview } from "../../redux/shop/shop.selectors";
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
+import { CollectionsOverviewContainer } from './collections-overview.styles';
 
-import './collections-overview.styles.scss';
-
-const CollectionsOverview = ({ collections }) => {
-    return (
-        <div className="collections-overview">
-            {
-                collections.map(({ id, ...otherCollectionProps }) => (
-                    <CollectionPreview key={id} {...otherCollectionProps} />
-                ))
-            }
-        </div>
-    );
-};
+const CollectionsOverview = ({ collections }) => (
+    <CollectionsOverviewContainer>
+        {collections.map(({ id, ...otherCollectionProps }) => (
+            <CollectionPreview key={id} {...otherCollectionProps} />
+        ))}
+    </CollectionsOverviewContainer>
+);
 
 const mapStateToProps = createStructuredSelector({
     collections: selectCollectionsForPreview
